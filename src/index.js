e628a606f5ac1e838a2467a551bb29305689b209
jayant <jayantsharma3228@gmail.com>
2024-04-26 13:03:08 +0530

    setup project files - part1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// nodemon kuch nhi hai jaise hi aapki file save hogi y aapke server ko restart kardega 
+//dev dependicies hoti hai jo devlopment ke time hum use karte hai 
+// produciton me dev dependicies ka koi use nhi hai ......cd
